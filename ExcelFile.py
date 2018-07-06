030fdc1ef40d4008e06584258f41cf00ffeec846
David Mendez Guardado <MendezDavidE@JohnDeere.com>
2018-07-06 14:07:50 -0500

    -add the styles to excel file.

--- ExcelFile.py
+++ ExcelFile.py
@@ -2,6 +2,7 @@
 # _*_coding: utf-8_*_
 
 from openpyxl import Workbook
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
 import datetime
 from DateDetails import *
 
@@ -12,7 +13,7 @@ class ExcelFile():
         self.file = Workbook()
         self.sheet = self.file.active
         self.details = DateDetails()
-        self.cells = ["B", "C", "D", "E", "F"]
+        self.cells = ["A", "B", "C", "D", "E", "F"]
         self.work_hours = "9.5"
 
     def set_datetime(self, datetime):
@@ -22,19 +23,49 @@ class ExcelFile():
         self.file.save(filename)
 
     def set_headers(self):
-        self.sheet['A1'] = self.date.strftime('%B')
+        self.sheet['A1'] = "Mes de " + self.date.strftime('%B')
         init_week, end_week = self.details.get_init_end_week(self.date.year, int(self.date.strftime('%V')), 4)
         self.sheet['A2'] = 'Week from ' + init_week.strftime('%d/%m/%y') + ' to ' + end_week.strftime('%d/%m/%y')
         self.sheet['A4'] = 'Activity'
         self.sheet['A5'] = 'Programación'
         for i in self.cells:
-            self.sheet[i + '4'] = init_week.strftime('%A') + ' ' + str(init_week.day)
-            day = init_week.day + 1
-            init_week = date(init_week.year, init_week.month, day)
+            if i != "A":
+                self.sheet[i + '4'] = init_week.strftime('%A') + ' ' + str(init_week.day)
+                day = init_week.day + 1
+                init_week = date(init_week.year, init_week.month, day)
 
     def set_data(self):
         for i in self.cells:
-            self.sheet[i + '5'] = self.work_hours
+            if i != "A":
+                self.sheet[i + '5'] = self.work_hours
+                self.sheet[i + '6'] = self.work_hours
 
     def get_week(self):
         return self.details.get_init_end_week(self.date.year, int(self.date.strftime('%V')), 4)
+
+    def set_styles(self):
+        a1 = self.sheet["A1"]
+        a2 = self.sheet["A2"]
+        ft = Font(name="Calibri", size="22", bold="True")
+        a1.font = ft
+        ft = Font(name="Calibri", size="16", bold="True")
+        a2.font = ft
+        ft = Font(name="Calibri", size="18", bold="True")
+        fill = PatternFill(fill_type="solid", start_color='5b9bd5', end_color='5b9bd5')
+        for i in self.cells:
+            cell = self.sheet[i + '4']
+            cell.font = ft
+            cell.fill = fill
+            if i != "A":
+                self.sheet.column_dimensions[i].width = 21.11
+            else:
+                self.sheet.column_dimensions[i].width = 17.56
+        ft = Font(name="Calibri", size="11")
+        fill = PatternFill(fill_type="solid", start_color='ffc000', end_color='ffc000')
+        for i in self.cells:
+            cell = self.sheet[i + '5']
+            cell.font = ft
+            cell = self.sheet[i + '6']
+            cell.font = ft
+            if i != "A":
+                cell.fill = fill
--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 from EmailDetails import *
+from ExcelFile import *
 
-email = EmailDetails()
-email.send_email()
+
+def create_file(filename):
+    file = ExcelFile()
+    file.set_datetime(datetime.datetime(2018, 6, 28))
+    file.set_headers()
+    file.set_data()
+    file.set_styles()
+    file.save_as(filename)
+    return file.get_week()
+
+
+filename = 'reporte_horas.xlsx'
+init_week, end_week = create_file(filename)
