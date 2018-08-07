20b7e00a1647a54d57aa22b16b5c1c0e4f2a47ae
David Mendez Guardado <MendezDavidE@JohnDeere.com>
2018-08-07 17:14:34 -0500

    revert

--- Test/test_mail.py
+++ Test/test_mail.py
@@ -1,19 +1,5 @@
-#!/usr/bin/env python
-# _*_coding: utf-8_*_
-
-from .ExcelFile import *
-from .EmailDetails import *
+from SimpleExcelFile.EmailDetails import *
 import datetime
-
-
-def create_file(filename):
-    file = ExcelFile()
-    file.set_datetime(datetime.datetime.now())
-    file.set_headers()
-    file.set_data()
-    file.set_styles()
-    file.save_as(filename)
-    return file.get_week()
 
 
 def send_email(fromaddr, toaddr, passwd, subject, body, filename):
@@ -21,6 +7,12 @@ def send_email(fromaddr, toaddr, passwd, subject, body, filename):
     email.send_email()
 
 
+year_init = 2018
+month_init = 7
+day_init = 23
+year_end = 2018
+month_end = 7
+day_end = 27
 filename = "reporte_horas.xlsx"
 passwd = '[password]'
 subject = 'Reporte de horas de la semana'
@@ -28,6 +20,6 @@ fromaddr = '[email]'
 toaddr = ['mail1', 'mail2', 'mailn']
 message = 'Hola un cordial saludo, soy [nombre], te adjunto el reporte de horas de la semana, hablando con la gente de Internacional de sistemas me dijeron que no habia problema si les enviaba el reporte desde este correo.'
 body = message
-init_week, end_week = create_file(filename)
-subject += " " + init_week.strftime('%d/%m/%y') + " a " + end_week.strftime('%d/%m/%y')
+subject += " " + datetime.datetime(year_init, month_init, day_init).strftime('%d/%m/%y') + " a " + datetime.datetime(
+    year_end, month_end, day_end).strftime('%d/%m/%y')
 send_email(fromaddr, toaddr, passwd, subject, body, filename)
